8a588b9d58c3c616d2b7f9668d6f059a9cb1f101
 mac <kksanaa@mail.ru>
2022-10-24 12:32:11 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+bool stop = false;
+int number = 0;
+int summ = 0;
+while (!stop)
+{
+    Console.WriteLine("Введите число");
+    number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+        summ++;
+    Console.WriteLine("Продолжить вводить числа ? y/n");
+    string result = Console.ReadLine();
+    if (result == "y")
+        stop = false;
+    else
+        stop = true;
+
+}
+Console.WriteLine($"Кол-во чисел, которые больше 0 - {summ}");
